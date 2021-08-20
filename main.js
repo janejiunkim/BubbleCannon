df89e73446c9776f02f7ae08bd3c6bc7e6959f3f
janejiunkim <janejiunkim@gmail.com>
2021-08-19 17:17:57 -0700

    so many issues

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ let angle = 0;
 // cannon
 let cannon;
 // all possible bubble colors
-const bubbleColors = ["pink", "powderblue","mediumpurple", "limegreen"];
+const bubbleColors = ["pink", "powderblue","mediumpurple", "limegreen","gold"];
+// array of all the bubbles
+const allBubbles = [];
 // ====================== SETUP FOR CANVAS RENDERING ======================= //
 // 2D rendering context for canvas element.
 // It is used for drawing shapes, text, images, and other objects.
@@ -34,6 +36,7 @@ class Cannon {
         this.centerX = (this.x + this.width)/2;
         this.centerY = (this.y + this.height)/2;
         this.centerPoint = (this.centerX, this.centerY);
+        this.hasBubble = true;
 
     this.render = () => {
         ctx.fillStyle = this.color;
@@ -51,8 +54,8 @@ class Bubble {
     constructor() {
         this.color = bubbleColors[randomColor()];
 
-    this.render = () => {
-
+    this.render = (x,y) => {
+        drawCircle(ctx, x, y, 18, this.color, "white", 2);
     }
     }
 
@@ -87,6 +90,87 @@ function drawCircle(ctx, x, y, radius, fill, stroke, strokeWidth) {
         ctx.stroke();
     }
 }
+// create a bubble to sit on top of cannon
+function loadCannon() {
+
+}
+// when user presses space, cannon will release ball 
+function shoot() {
+
+}
+
+function createBubbleRows(){
+    // let x = 33;
+    // let y = 35;
+    // 6 rows of bubbles; 1st has 10, 2nd has 11, 3rd has 10 ....
+    for (let i=0; i < 6; i++){
+        let tempArr = [];
+        // depending on odd or even row push 10 or 11 bubbles into an array
+        if (i % 2 === 0) {
+            for(let j = 0; j < 10; j++) {
+                let temp = new Bubble();
+                tempArr.push(temp);
+            }
+        } else {
+            for (let j=0; j < 11; j++) {
+                let temp = new Bubble();
+                tempArr.push(temp);
+            }
+            
+        }
+        allBubbles.push(tempArr);
+    }
+}
+
+function detectHit(p1, p2) {
+    let hitTest = (
+        p1.y + p1.
+    )
+}
+/*
+function renderBubbles(){
+    let x = 33;
+    let y = 35;
+    for(let i=0; i < allBubbles.length; i++){
+        // iterate through array and render each bubble?
+        allBubbles[i].render(x, y);
+        if (x < game.width -40) {
+            x += 38;
+        } else {
+            x = 33;
+            y += 38;
+        }
+    }
+
+}
+*/
+
+function renderBubbles(){
+    let y =35;
+    for(let i=0; i< allBubbles.length; i++) {
+        if (i%2 === 0) {
+            let x= 38;
+            for(let j= 0; j < allBubbles[i].length; j++) {
+                allBubbles[i][j].render(x,y);
+                if (x < game.width -40) {
+                    x += 38;
+                }
+            }
+            y += 33;
+        } else {
+            let x=20;
+            for (j=0; j < allBubbles[i].length; j++){
+                allBubbles[i][j].render(x,y);
+                if (x < game.width-40){
+                    x += 38;
+                }
+            }
+            y += 33;
+
+        }
+
+    }
+}
 // black circle with a red stroke (width 2) at coordinates 50,50, with radius 25
 // let ctx = canvas.getContext('2d')
 //drawCircle(ctx, 50, 50, 25, 'black', 'red', 2)
@@ -98,16 +182,26 @@ function drawCircle(ctx, x, y, radius, fill, stroke, strokeWidth) {
 /**
  * @function gameLoop
  * @todo clear the canvas
+ * @todo generate bubbles
  * @todo move cannon left and right, shoot with space bar
+ * @todo if hit detected, drop bubbles of same color
  */
+
 setInterval(function () {
+    console.log("set interval");
     ctx.save();
     ctx.clearRect(0, 0, game.width, game.height);
-
+    //let newBubz = new Bubble();
+    //console.log(newBubz);
+    //newBubz.render();
+    //createBubbles();
+    renderBubbles();
     drawCannon();
+   
 
     ctx.restore();
 }, 100);
+
 
 function drawCannon() {
     //ctx.translate(175, 525);
@@ -133,6 +227,12 @@ function drawCannon() {
 // EVENT LISTENERS
 window.addEventListener("DOMContentLoaded", (e) => {
     cannon = new Cannon('black');
+    //let newBubz = new Bubble();
+    //console.log(newBubz);
+    //newBubz.render();
+    createBubbleRows();
+
+   //drawCannon();
     document.addEventListener('keydown', function (event) {
         if (event.key === 'a') {
             if (angle > -80) {
